73f3aefc0c44093ae433a2fab200776bb754361d
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-09-11 21:55:13 +0700

    Update files

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,20 @@ const Border = () => {
   return (
     <main className="border-2 broder-solid border-green-500 flex flex-col min-h-screen">
       <header className="border-2 border-oslid border-green h-[4rem]">
-        <h1 className="text-center font-bold text-[32px]">Header section</h1>
+        <h1 className="text-center font-semibold text-[32px]">
+          Header section
+        </h1>
       </header>
       <main className="border-2 border-solid border-red-500 flex flex-1">
         <aside className="border-2 border-solid border-green-500 w-[20%] flex">
-          <h1 className="m-auto font-bold text-[32px]">Side</h1>
+          <h1 className="m-auto font-semibold text-[32px]">Side</h1>
         </aside>
         <div className="border-2 border-solid border-red-500 flex-1 flex flex-col">
           <div className="border-2 border-solid border-green-500 flex-1 flex">
             {new Array(3).fill(0).map((_, idx) => (
               <section
                 className={`border-2 border-solidh-[10rem] w-full flex ${
-                  idx === 0
-                    ? "bg-blue-500"
-                    : idx === 1
+                  idx === 1
                     ? "bg-green-500"
                     : idx === 2
                     ? "bg-red-500"
@@ -37,7 +37,9 @@ const Border = () => {
         </div>
       </main>
       <footer className="border-2 border-oslid border-green h-[4rem] my-auto">
-        <h1 className="text-center font-bold text-[32px]">Footer section</h1>
+        <h1 className="text-center font-semibold text-[32px]">
+          Footer section
+        </h1>
       </footer>
     </main>
   );
