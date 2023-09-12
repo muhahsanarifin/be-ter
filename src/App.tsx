68a546c6c0acd0419ea992af3a33865be90a9255
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-09-12 09:38:46 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,43 @@
 const Border = () => {
   return (
-    <main className="border-2 broder-solid border-green-500 flex flex-col min-h-screen">
-      <header className="border-2 border-oslid border-green h-[4rem]">
-        <h1 className="text-center font-semibold text-[32px]">
+    <main className="flex flex-col min-h-screen">
+      <header className="h-[4rem] bg-yellow-100">
+        <h1 className="text-center font-medium text-[32px]">
           Header section
         </h1>
       </header>
-      <main className="border-2 border-solid border-red-500 flex flex-1">
-        <aside className="border-2 border-solid border-green-500 w-[20%] flex">
-          <h1 className="m-auto font-semibold text-[32px]">Side</h1>
+      <main className="flex flex-1">
+        <aside className="w-[20%] flex bg-red-200">
+          <h1 className="m-auto font-medium text-[32px]">Side</h1>
         </aside>
-        <div className="border-2 border-solid border-red-500 flex-1 flex flex-col">
-          <div className="border-2 border-solid border-green-500 flex-1 flex">
+        <div className="flex-1 flex flex-col">
+          <div className="flex-1 flex">
             {new Array(3).fill(0).map((_, idx) => (
               <section
-                className={`border-2 border-solidh-[10rem] w-full flex ${
+                className={`w-full flex ${
                   idx === 1
-                    ? "bg-green-500"
+                    ? "bg-green-200"
                     : idx === 2
-                    ? "bg-red-500"
-                    : "bg-blue-500"
+                    ? "bg-red-200"
+                    : "bg-blue-200"
                 }`}
                 key={idx}
               >
-                <h1 className="text-[32px] m-auto font-semibold">
+                <h1 className="text-[32px] m-auto font-medium">
                   Section {idx + 1}
                 </h1>
               </section>
             ))}
           </div>
-          <div className="border-2 border-solid border-green-500 h-[10rem]">
-            <section className="border-2 border-solid border-green-500 h-[10rem] flex">
-              <h1 className="text-[32px] m-auto font-semibold">Section 4</h1>
+          <div className="h-[10rem]">
+            <section className="h-[10rem] flex bg-purple-200">
+              <h1 className="text-[32px] m-auto font-medium">Section 4</h1>
             </section>
           </div>
         </div>
       </main>
-      <footer className="border-2 border-oslid border-green h-[4rem] my-auto">
-        <h1 className="text-center font-semibold text-[32px]">
+      <footer className="h-[4rem] my-auto bg-blue-100">
+        <h1 className="text-center font-medium text-[32px]">
           Footer section
         </h1>
       </footer>
